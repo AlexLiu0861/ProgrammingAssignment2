29ef5c6335d292aaaf33cf82e2e2af42be312b63
Yi Liu <liuyibuaa08@gmail.com>
2018-03-25 13:08:08 +0800

    Week 3's PA in Coursera.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a group of functions used to solve the inverse of matrix based on caching it.
 
-## Write a short comment describing this function
+## The first function is used to cache original matrix. It just likes the example given by Roger Peng.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+        setsolve = setsolve , getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## The next one is used to solve the inverse directly.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <<- x$getsolve()
+    if (!is.null(m)) {
+        message("Getting cached matrix")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
