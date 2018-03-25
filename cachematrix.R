edfca240a29791c5c1985ff08a33b321a1113396
Yi Liu <liuyibuaa08@gmail.com>
2018-03-25 13:29:26 +0800

    Add some explanation.

--- cachematrix.R
+++ cachematrix.R
@@ -7,12 +7,13 @@ makeCacheMatrix <- function(x = matrix()) {
     set <- function(y) {
         x <<- y
         m <<- NULL
-    }
-    get <- function() x
-    setsolve <- function(solve) m <<- solve
-    getsolve <- function() m
+    }#This little func is used to cache X, the origin.
+    get <- function() x 
+    setsolve <- function(solve) m <<- solve # Use to solved cache first time.
+    getsolve <- function() m #Cache the inverse if it exists, and it will be examined in the next func.
     list(set = set, get = get,
-        setsolve = setsolve , getsolve = getsolve)
+        setsolve = setsolve, getsolve = getsolve)
+    #Make those four as a list, so it can be used in cacheSolve function.
 }
 
 
@@ -24,7 +25,8 @@ cacheSolve <- function(x, ...) {
     if (!is.null(m)) {
         message("Getting cached matrix")
         return(m)
-    }
+    }# If we have solved, we can just return the final result.
+    #If the cache doesn't contain the inverse, we need to solve it another time.
     data <- x$get()
     m <- solve(data, ...)
     x$setsolve(m)
